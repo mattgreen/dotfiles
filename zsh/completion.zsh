5d8cefcf4bacf527f58907de78c38cdb4fc6b501
Matt Green <mattgreenrocks@gmail.com>
2013-11-08 13:36:01 -0800

    Case-insensitive completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,7 +3,7 @@ export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # Case-insensitive matching
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # Pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
