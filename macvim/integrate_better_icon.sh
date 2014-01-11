735854ee1ff08cbf96e4c23ade8f10bd65ea708a
Matt Green <mattgreenrocks@gmail.com>
2014-01-11 07:26:32 -0500

    Update mvim icon replacement script

--- macvim/integrate_better_icon.sh
+++ macvim/integrate_better_icon.sh
@@ -2,14 +2,26 @@
 #
 # Integrates a better MacVim logo
 #
-target=$(find /usr/local/Cellar -name MacVim.app 2> /dev/null | sort | tail -n 1)
+
+target="/Applications/MacVim.app"
+
+if [ ! -d "$target" ]; then
+    target=$(find /usr/local/Cellar -name MacVim.app 2> /dev/null | sort | tail -n 1)
+fi
+
+if [ "$target" == "" ]; then
+    echo "ERROR: can't find MacVim installation (checked /Applications, /usr/local/Cellar)"
+    exit 1
+fi
+
 path="$target/Contents/Resources/"
 
 wget -O /tmp/MacVim.icns http://dribbble.com/shots/337065-MacVim-Icon-Updated/attachments/15582
 
+echo "Replacing icon at $path..."
 cp /tmp/MacVim.icns $path
 
 echo
 echo
-echo "Please restart MacVim to see the new icon"
+echo "Please restart to see the new icon"
 
