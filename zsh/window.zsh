4e42959676736767a877d79131adb55c0d68555b
Matt Green <mattgreenrocks@gmail.com>
2014-02-07 10:08:57 -0500

    Just show basename on tab titles

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -1,12 +1,8 @@
 precmd() {
     local currentpath=${PWD/${HOME}/\~}
-    local tabtitle="$currentpath"
+    local tabtitle=$(basename "$currentpath")
 
-    if [[ ${#tabtitle}>24 ]] then
-        tabtitle=$(basename "$tabtitle")
-    fi
-
-    echo -ne "\e]1;${tabtitle: -24}\a"       # Tab title (max 24 chars)
+    echo -ne "\e]1;${tabtitle: -24}\a"  # Tab title (max 24 chars)
     echo -ne "\e]2;${currentpath}\a"    # Window title
 }
 
