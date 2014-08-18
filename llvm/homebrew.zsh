f6797d2ea8119a88c7ac93fbe7968d631b669562
Matt Green <mattgreenrocks@gmail.com>
2014-08-18 17:12:19 -0400

    Optimize by hardcoding everything

--- llvm/homebrew.zsh
+++ llvm/homebrew.zsh
@@ -1,10 +1,7 @@
-# Append LLVM toolchain to PATH
-if hash brew 2>/dev/null; then
-    LLVM_BIN_PATH=$(brew --cellar llvm33)/3.3/lib/llvm-3.3/bin
+LLVM_BIN_PATH=/usr/local/Cellar/llvm33/3.3_1/lib/llvm-3.3/bin
 
-    if [[ -d $LLVM_BIN_PATH ]]; then
-        path=($LLVM_BIN_PATH $path)
-    fi
-
-    unset LLVM_BIN_PATH
+if [[ -d $LLVM_BIN_PATH ]]; then
+    path=($LLVM_BIN_PATH $path)
 fi
+
+unset LLVM_BIN_PATH
