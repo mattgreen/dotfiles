d2caa67ad310138420a2f4ee162f041a80a1d816
Matt Green <mattgreenrocks@gmail.com>
2019-10-09 13:51:51 -0400

    2019 update

--- zsh/haskell.zsh
+++
@@ -1,7 +0,0 @@
-if [[ -d ~/.cabal/bin ]]; then
-    path=(~/.cabal/bin $path)
-fi
-
-if [[ -d ~/.local/bin ]]; then
-    path=(~/.local/bin $path)
-fi
--- zsh/rust.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -d ~/.cargo/bin ]]; then
-    path=(~/.cargo/bin $path)
-fi
