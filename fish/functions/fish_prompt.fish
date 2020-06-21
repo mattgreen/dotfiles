432e024ae2dd65178644b24f503a5b17cc8e93ac
Matt Green <mattgreenrocks@gmail.com>
2020-06-21 14:43:31 -0400

    Change dirty indicator

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,7 +5,7 @@ if status is-interactive
       set branch (command git rev-parse --short HEAD 2>/dev/null)
     end
 
-    set_color blue; echo -sn $branch; set_color normal
+    echo -sn $branch
   end
 
   function __prompt_git_status
@@ -26,48 +26,18 @@ if status is-interactive
       set dirty " "
 
       if test "$$prompt_result" != "0"
-        set dirty "*"
+        set dirty "•"
       end
 
       set -e __prompt_git_static
       set -e $prompt_result
     end
 
-    printf " on %s %s" $branch $dirty
+    echo -n " on "
+    set_color blue
+    echo -n $branch $dirty
+    set_color normal
   end
-
-  # function __prompt_git_dirty
-  #   if set --query __prompt_dirty_checking_$fish_pid
-  #     return 0
-  #   end
-
-  #   set --local uid (random)
-  #   set --local cmd "sleep 1; git status -unormal --porcelain --ignore-submodules 2>/dev/null | wc -l | sed 's/^ *//g'"
-  #   set --universal __prompt_result_$uid ""
-
-  #   function on_prompt_finish_$uid --on-variable __prompt_result_$uid -V uid
-  #     set fish_bind_mode "insert"
-  #     set fish_bind_mode "default"
-  #     functions -e on_prompt_finish_$uid
-  #   end
-
-  #   fish -c "sleep 1; set -U __prompt_result_$uid (1); kill -WINCH $fish_pid" >/dev/null 2>&1 &
-
-  #   # async_run "__prompt_dirty_checking_$fish_pid" __prompt_git_dirty_finish $cmd
-
-
-
-  #   if set --query __prompt_result_$uid
-  #     if test "$__prompt_result_$uid" -eq 0
-  #       echo -sn "  "
-  #     else
-  #       echo -sn " d"
-  #     end
-
-  #     set --erase "__prompt_result_$uid"
-  #     set -U "__prompt_dirty_checking_$fish_pid"
-  #   end
-  # end
 
   function fish_prompt
       echo ''
