87fd30e21a28e7f0063e05606a184110fc21e842
Matt Green <mattgreenrocks@gmail.com>
2020-08-05 22:24:19 -0400

    Add Homebrew env vars

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,7 @@ end
 # Use nvim as editor if available
 set -gx EDITOR vim
 command -q nvim && set -gx EDITOR nvim
+
+# Homebrew config
+set -gx HOMEBREW_NO_EMOJI 1
+set -gx HOMEBREW_NO_ANALYTICS 1
