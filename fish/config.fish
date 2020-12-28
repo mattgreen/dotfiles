aa260116b6ddaf6656aed69a1661a096c45e8445
Matt Green <mattgreenrocks@gmail.com>
2020-12-28 13:21:30 -0500

    Add Rust to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,8 @@ command -q nvim && set -gx EDITOR nvim
 # Homebrew config
 set -gx HOMEBREW_NO_EMOJI 1
 set -gx HOMEBREW_NO_ANALYTICS 1
+
+# Rust
+if test -d "$HOME/.cargo"
+    set --export PATH "$HOME/.cargo/bin" $PATH
+end
