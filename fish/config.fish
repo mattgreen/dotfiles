e60c0194a4a309b37e34116bc126e8e6f2865a79
Matt Green <mattgreenrocks@gmail.com>
2022-11-08 16:32:06 -0500

    Reduce use of color, use fish_add_path, source machine local config

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,23 @@
 set -gx EDITOR vim
 command -q nvim && set -gx EDITOR nvim
 
+# Be intentional about use of color
+set fish_color_operator normal
+
 # Homebrew config
 set -gx HOMEBREW_NO_EMOJI 1
 set -gx HOMEBREW_NO_ANALYTICS 1
 
+# Haskell
+fish_add_path --path --append "$HOME/.ghcup/bin"
+fish_add_path --path --append "$HOME/.cabal/bin"
+
 # Rust
-if test -d "$HOME/.cargo"
-    set --export PATH "$HOME/.cargo/bin" $PATH
+fish_add_path --path --append "$HOME/.cargo/bin"
+
+# Source machine-local config if it exists
+set --local FISH_CONFIG_ROOT (dirname (status --current-filename))
+if test -f "$FISH_CONFIG_ROOT/config.local.fish"
+    source "$FISH_CONFIG_ROOT/config.local.fish"
 end
+
