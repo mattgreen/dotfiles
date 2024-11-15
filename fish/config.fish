5709f0ae35b28eb1b5558582749d8d6efc2f0507
Matt Green <mattgreenrocks@gmail.com>
2024-11-15 14:15:39 -0500

    2024 Q4 update

--- fish/config.fish
+++ fish/config.fish
@@ -8,9 +8,18 @@ set fish_color_operator normal
 # Homebrew config
 set -gx HOMEBREW_NO_EMOJI 1
 set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
+
+# Ruby
+fish_add_path --path --prepend "/usr/local/opt/ruby/bin"
 
 # .NET
+set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
 fish_add_path --path --append "$HOME/.dotnet/tools"
+
+# Godot
+set -gx GODOT "$HOME/Library/Application Support/godotenv/godot/bin/godot"
+fish_add_path --path --append "$HOME/Library/Application Support/godotenv/godot/bin"
 
 # Haskell
 fish_add_path --path --append "$HOME/.ghcup/bin"
