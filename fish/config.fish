2952ce8db4a81d32874aa308b00417369163e08f
Matt Green <mattgreenrocks@gmail.com>
2026-05-20 20:16:10 -0400

    Extract fish theme

--- fish/config.fish
+++ fish/config.fish
@@ -2,45 +2,6 @@
 set -gx EDITOR vim
 command -q nvim && set -gx EDITOR nvim
 
-# Set theme
-set --global fish_color_autosuggestion brblack
-set --global fish_color_cancel
-set --global fish_color_command
-set --global fish_color_comment brblack
-set --global fish_color_cwd normal
-set --global fish_color_cwd_root normal
-set --global fish_color_end
-set --global fish_color_error brred
-set --global fish_color_escape
-set --global fish_color_history_current
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_keyword
-set --global fish_color_normal normal
-set --global fish_color_operator
-set --global fish_color_option
-set --global fish_color_param
-set --global fish_color_quote green
-set --global fish_color_redirection
-set --global fish_color_search_match --reverse
-set --global fish_color_selection --reverse
-set --global fish_color_status normal
-set --global fish_color_user normal
-set --global fish_color_valid_path
-set --global fish_pager_color_background
-set --global fish_pager_color_completion normal
-set --global fish_pager_color_description brblack
-set --global fish_pager_color_prefix --underline=single
-set --global fish_pager_color_progress brblack
-set --global fish_pager_color_secondary_background
-set --global fish_pager_color_secondary_completion
-set --global fish_pager_color_secondary_description
-set --global fish_pager_color_secondary_prefix
-set --global fish_pager_color_selected_background --reverse
-set --global fish_pager_color_selected_completion
-set --global fish_pager_color_selected_description
-set --global fish_pager_color_selected_prefix
-
 # Homebrew config
 fish_add_path --path --prepend "/opt/homebrew/bin"
 set -gx HOMEBREW_NO_EMOJI 1
