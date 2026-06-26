570f826f1ebf55a4606e5aed7a82955445c89a9b
Matt Green <mattgreenrocks@gmail.com>
2026-06-25 22:34:04 -0400

    Fix code launcher on Linux

--- fish/functions/code.fish
+++ fish/functions/code.fish
@@ -2,6 +2,6 @@ function code
     if test "$(uname)" = 'Darwin'
         env VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $argv
     else
-        env VSCODE_CWD="$PWD" command code $argv
+        env VSCODE_CWD="$PWD" code $argv
     end
 end
